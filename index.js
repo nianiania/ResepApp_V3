e689fe2463c5f3293a85d3d0ccc0d9660d6eee1f
kurnia22 <kurniawati.22.nk@gmail.com>
2017-05-21 00:38:46 +0700

    fixing singup

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var cookieParser = require('cookie-parser')
 app.use(bodyParse.urlencoded({ extended: true }));
 app.use(bodyParse.json());
 
-app.use(function(req,res, next) {
+app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Header", "Origin, X-Requested-with, Content-Type, Accept");
     next();
@@ -24,7 +24,7 @@ app.set('view engine', '.hbs')
 const Pool = require('pg').Pool;
 var config = {
     user: 'postgres',
-    database: 'resep',
+    database: 'resep_v2',
     password: 'postgres',
     host: 'localhost',
     port: 5432,
@@ -35,6 +35,21 @@ process.on('unhandledRejection', function(e) {
     console.log(e.message, e.stack)
 })
 var pool = new Pool(config)
+
+
+//============================ setup Firebase =================================
+var admin = require("firebase-admin");
+var serviceAccount = require("./resepv2-d3f60-firebase-adminsdk-e95wy-bd57a737c5.json");
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://resepv2-d3f60.firebaseio.com"
+});
+
+var firebase = require("firebase");
+firebase.initializeApp({
+    apiKey: " AIzaSyCUgo5_dMpN03C6ZsiAlS-kjEZH1RkE9_4",
+    databaseURL: "https://resepv2-d3f60.firebaseio.com"
+});
 
 //==================routing==============
 
@@ -59,7 +74,44 @@ app.route('/signup')
         res.render('signup')
     })
     .post(function(req, res) {
+        var first_name = req.body.first_name
+        var last_name = req.body.last_name
+        var email = req.body.email
+        var password = req.body.password
+        var confirm_password = req.body.confirm_password
 
+        if (password !== confirm_password) {
+            var error_msg = "Your password and  confirmation password should be the same!"
+            res.send(error_msg)
+        } else {
+            signup();
+        }
+
+        function signup(){
+            firebase.auth().createUserWithEmailAndPassword(email, password)
+                .then(function(result) {
+                    console.log('Emailnya:', result.email)
+                    console.log('UID:', result.uid)
+                    var uid = result.uid
+                    var email_signup = result.email
+
+                    var query_post = 'insert into db_user(uid, email)' + 'values($1,$2)'
+
+                    pool.query(query_post, [uid, email])
+                        .then((result) => {
+                            console.log('succes insert data');
+                            res.redirect('/signin')
+                        })
+                        .catch((err) => {
+                            console.log('error running query', err);
+                        })
+                })
+                .catch((err) => {
+                    var errorCode = error.code;
+                    var errorMessage = error.message;
+                        res.send(errorMessage)
+                })
+        }
     })
 app.route('/signin')
     .get(function(req, res) {
@@ -70,11 +122,18 @@ app.route('/signin')
     })
 
 //=============webserver============================
+app.listen(4000, function(){
+    pool
+        .query('CREATE TABLE IF NOT EXISTS resep(id SERIAL PRIMARY KEY, nama_resep VARCHAR(40) not null, deskripsi VARCHAR(70) not null, penulis VARCHAR(40) not null, cara_pembuatan VARCHAR(700) not null)')
+        .then(function(){
+            console.log('Table resep is exist!')
+        })
+    pool
+        .query('CREATE TABLE IF NOT EXISTS db_user(id SERIAL PRIMARY KEY, uid VARCHAR(80) not null, email VARCHAR(80) not null)')
+        .then(function() {
+            console.log('Table db_user is exist!')
+        })
+        console.log('Server is listening on 4000 and Table is exist!')
+})
 
-pool
-	.query('CREATE TABLE IF NOT EXISTS resep(id SERIAL PRIMARY KEY, nama_resep VARCHAR(40)not null,deskripsi VARCHAR(70)not null, penulis VARCHAR(40)not null, cara_pembuatan VARCHAR(500)not null)')
-	.then(function(){
-		app.listen(4000, function(){
-			console.log('server is listening on 4000')
-		})
-	})
+
